35257c8a9e64e024165dc218951c04ed2b301738
RanikaR <rmr29@njit.edu>
2017-02-20 16:39:17 -0500

    Added updated files
    Updated category list
    Create add_category_form

--- category_list.php
+++ category_list.php
@@ -1,14 +1,3 @@
-<?php
-require_once('database.php');
-
-// Get all categories
-$query = 'SELECT * FROM categories
-                       ORDER BY categoryID';
-$statement = $db->prepare($query);
-$statement->execute();
-$categories = $statement->fetchAll();
-$statement->closeCursor();
-?>
 <!DOCTYPE html>
 <html>
 
@@ -62,16 +51,21 @@ $statement->closeCursor();
     </table>
 
     <h2>Add Category</h2>
-    
     <!-- add code for the form here -->
-    
-    <br>
+	<form action="add_category.php" method="post"
+		  id="add_category_form">
+		  
+		  <label>Name:</label>
+		  <input type="text" "name" />
+		  <input id="add_category_button" type="submit" value="Add" />
+	</form>
+	
     <p><a href="index.php">List Products</a></p>
 
     </main>
 
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Guitar Shop, Inc.</p>
+        <p>&copy; 2017 My Guitar Shop, Inc.</p>
     </footer>
 </body>
 </html>
